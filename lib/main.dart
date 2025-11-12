7c6b43817ae479bdff8cbdb4e2938973d1b581fd
Michał Kuzemczak <kuzemczak.michal@gmail.com>
2025-11-12 19:04:08 +0100

    actually play sound

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sounboard/screens/sound_test.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,7 +31,8 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MultiPlayback(),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
