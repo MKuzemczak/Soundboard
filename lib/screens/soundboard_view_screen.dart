b13747eead92c78f9a55cc546e76be63966aae0e
Michał Kuzemczak <kuzemczak.michal@gmail.com>
2025-11-14 23:54:02 +0100

    soundboard  view nicer alignment and app bar

--- lib/screens/soundboard_view_screen.dart
+++ lib/screens/soundboard_view_screen.dart
@@ -1,7 +1,10 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:sounboard/database/db.dart';
 import 'package:sounboard/database/sound_containter_details.dart';
+import 'package:sounboard/database/sound_details.dart';
 import 'package:sounboard/database/soundboard_details.dart';
+import 'package:stretch_wrap/stretch_wrap.dart';
 
 class SoundboardViewScreen extends StatefulWidget {
   final SoundboardDetails soundboardDetails;
@@ -30,51 +33,76 @@ class _SoundboardViewScreenState extends State<SoundboardViewScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.soundboardDetails.name),
+        backgroundColor: Color.fromARGB(255, 58, 86, 67),
+      ),
       body: Column(
         children: [
           Expanded(
             child: FutureBuilder(
               future: _soundContainersFuture,
-              builder: (context, snapshot) {                
+              builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return const Center(child: CircularProgressIndicator());
                 } else if (snapshot.hasError) {
                   return Center(child: Text('Error: ${snapshot.error}'));
                 } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return const Center(child: Text('No sound containers found.'));
+                  return const Center(
+                    child: Text('No sound containers found.'),
+                  );
                 }
 
                 final soundContainers = snapshot.data!;
 
-                return SingleChildScrollView(
-                  child: Column(
-                    children: _getCountContainerButtonRows(soundContainers),
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: StretchWrap(
+                    spacing: 8.0,
+                    runSpacing: 8.0,
+                    autoStretch: AutoStretch.all,
+                    children: List.generate(
+                      soundContainers.length,
+                      (i) => ElevatedButton(
+                        style: ButtonStyle(backgroundColor: WidgetStateProperty.all<Color>(Color.fromRGBO(0, 0, 0, 1))),
+                        onLongPress: () async {
+                          FilePickerResult? result = await FilePicker.platform.pickFiles();
+                  
+                          if (result != null) {
+                            print(result.files.single.path!);
+                            final uri1 = result.files.single.path!;
+                            String s = "$uri1 picked";
+                            final dbHelper = DbHelper();
+                            final soundDetails = await dbHelper.insertSound(SoundDetails(name: uri1.split("\\").last, path: uri1));
+                            await dbHelper.insertSoundContainerToSoundMapping(soundContainers[i], soundDetails);
+                            s = "$s and inserted!";
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text(s)),
+                            );
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text("Not picked")),
+                            );
+                          }
+                        },
+                        onPressed: () async {
+                          final dbHelper = DbHelper();
+                          final soundDetails = await dbHelper.getSounds(soundContainerDetails: soundContainers[i]);
+                          
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text("${List.generate(soundDetails.length, (i) => soundDetails[i].name)}")),
+                          );
+                        },
+                        child: Text(soundContainers[i].name),
+                      ),
+                    ),
                   ),
                 );
-              }
-            )
-          )
+              },
+            ),
+          ),
         ],
-      )
+      ),
     );
-  }
-
-  List<Widget> _getCountContainerButtonRows(List<SoundContainerDetails> soundContainers) {
-    List<Widget> result = [];
-
-    for (var i = 0; i < soundContainers.length;) {
-      List<Widget> rowChildren = [];
-      for (var j = 0; j < 5 && i < soundContainers.length; {j++, i++}) {
-        rowChildren.add(
-          ElevatedButton(
-            onPressed: () {},
-            child: Text(soundContainers[i].name)
-          )
-        );
-      }
-      result.add(Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: rowChildren));
-    }
-
-    return result;
   }
 }
